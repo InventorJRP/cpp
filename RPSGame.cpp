46d85b7e9bc86f35ac0e0b03a9316d2bdf214cad
InventorJRP <jrp@live.ca>
2024-10-03 12:39:13 -0700

    Rock, Paper Scissors game first repo

---
+++ RPSGame.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+
+char getUserChoice();
+char getComputerChoice();
+void chooseWinner(char player, char computer);
+bool playAgain();
+
+int main(){
+
+    char player;
+    char computer;
+    bool restart;
+
+    std::cout << "ROCK, PAPER, SCISSORS GAME\n";
+
+    do{
+        char player = getUserChoice();
+        char computer = getComputerChoice();
+        chooseWinner(player, computer);
+        bool restart = playAgain();
+    }while(restart == true);
+    
+    std::cout << "Thanks for Playing!";
+
+    return 0;
+}
+
+char getUserChoice(){
+
+    char choice
+
+    std::cout << "Choose Rock(R), Paper(P), or Scissors(S)\n Enter Choice: ";
+    std::cin >> choice;
+
+    while(choice != R || choice != P || choice != S){
+        std::cout << "Invalid choice!\n Enter only (R), (P), or (S): ";
+        std::cin >> choice;
+    }
+    return choice;
+}
+char getComputerChoice(){
+
+    char choice
+
+    srand(time(0));
+    int num = rand() % 3;
+
+    switch(num){
+        case 0: choice = 'R';
+                break;
+        case 1: choice = 'P';
+                break;
+        case 2: choice = 'S';
+                break;
+    }
+    return choice;
+}
+void chooseWinner(char player, char computer){
+    
+    switch(player){
+        
+         case 'R': switch(computer){
+                
+                case 'R':
+                    std::cout << "Computer: R \nDraw, Try Again";
+                    break;
+                case 'P':
+                    std::cout << "Computer: P \nYou Lose, Try Again!";
+                    break;
+                case 'S':
+                    std::cout << "Computer: S \nYou Win!";
+                    break;}
+            break;
+        
+        case 'P': switch(computer){
+                
+                case 'R':
+                    std::cout << "Computer: R \nYou Win!";
+                    break;
+                case 'P':
+                    std::cout << "Computer: P \nDraw, Try Again!";
+                    break;
+                case 'S':
+                    std::cout << "Computer: S \nYou Lose, Try Again!";
+                    break;}
+            break;
+        case 'S': switch(computer){
+                
+                case 'R':
+                    std::cout << "Computer: S \nYou Lose, Try Again!";
+                    break;
+                case 'P':
+                    std::cout << "Computer: P \nYou Win!";
+                    break;
+                case 'S':
+                    std::cout << "Computer: S \nDraw, Try Again!";
+                    break;}
+            break;
+}
+}
+bool playAgain(){
+
+    char choice;
+    
+    do{
+        std::cout << "Would you like to play again?\n(y/n): ";
+        std::cin >> choice;
+
+        if(choice != 'y' || choice != 'n'){
+            std::cout << "Invalid!\n";
+    }
+    }while(choice != 'y' || choice != 'n')
+    
+    if(choice == 'y'){
+        return true;
+    }else{
+        return false;
+    }
+}
